afbda4fbed4aba6e3751762abd6dc21c3c8dcaef
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-12 18:05:59 +0100

    Refactoring

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import data from './data';
 
 export default function App() {
   const cards = data.map((item) => {
-    return <Card item={item} />;
+    return <Card {...item} />;
   });
 
   return (
     <div className="container">
       <Header />
-      {cards}
+      <div className="cards">{cards}</div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,24 @@ import locationIcon from '../images/location-icon.png';
 export default function Card(props) {
   return (
     <div className="card">
-      <img src={props.item.imageUrl} className="card--image" />
+      <img src={props.imageUrl} className="card--image" />
       <div className="card--info">
         <img src={locationIcon} className="card--icon" />
-        <span className="card--location">{props.item.location}</span>
+        <span className="card--location">{props.location}</span>
         <span>
           <a
-            href={`${props.item.googleMapsUrl}`}
+            href={`${props.googleMapsUrl}`}
             target="_blank"
             className="card--link"
           >
             View on Google Maps
           </a>
         </span>
-        <h1 className="card--title">{props.item.title}</h1>
+        <h1 className="card--title">{props.title}</h1>
         <p className="card--dates">
-          {props.item.startDate} - {props.item.endDate}
+          {props.startDate} - {props.endDate}
         </p>
-        <p className="card--description">{props.item.description}</p>
+        <p className="card--description">{props.description}</p>
       </div>
     </div>
   );
